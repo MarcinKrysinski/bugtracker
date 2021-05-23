857bc48d4f566d529b5f1eb63a88b4f750798993
Marcin <marcin.jakubkrysinski@gmail.com>
2021-05-23 16:10:44 +0200

    bugtracker-31 Soft delete użytkowników

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function (){
+
+    $('.table .delBtn').on('click', function (event){
+        event.preventDefault();
+        let href = $(this).attr('href');
+        $('#myModal #delRef').attr('href', href);
+        $('#myModal').modal();
+    });
+});
